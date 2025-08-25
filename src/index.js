322bae00ad57b4da70c6d441e6b3150b5dfc0337
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2025-08-25 15:15:15 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,38 @@
 class GuessingGame {
-  constructor() {
-    throw new Error('Not implemented');
+  arr = [];
+
+  left = 0;
+
+  right = 0;
+
+  mid = 0;
+
+  setRange(min, max) {
+    for (let index = min + 1; index < max; index += 1) {
+      this.arr.push(index);
+    }
+    this.right = this.arr.length - 1;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  guess() {
+    if (this.left <= this.right) {
+      this.mid = Math.ceil((this.right + this.left) / 2);
+    }
+    return this.arr[this.mid];
+  }
 
-  lower() { }
+  lower() {
+    this.right = this.mid - 1;
+  }
 
-  greater() { }
-*/
+  greater() {
+    this.left = this.mid + 1;
+  }
 }
 
 module.exports = GuessingGame;
+
+const arr = [1, 2, 3, 4, 5];
+
+const rez = arr.map((item) => item + 1);
+rez.reduce((acc, item) => acc + item);
